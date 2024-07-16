c1ae623729bdd8c07f24ef78693ef8f3811205de
Manuel <manuelbuenog9@gmail.com>
2024-07-16 16:44:31 +0200

    Finishing project

--- src/main/java/com/manbuegom/srv_backend/controller/TeacherController.java
+++ src/main/java/com/manbuegom/srv_backend/controller/TeacherController.java
@@ -1,11 +1,15 @@
 package com.manbuegom.srv_backend.controller;
 
+import com.manbuegom.srv_backend.exception.ResourceNotFoundException;
 import com.manbuegom.srv_backend.model.Teacher;
 import com.manbuegom.srv_backend.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin("*")
@@ -23,5 +27,34 @@ public class TeacherController {
     @PostMapping("/teachers")
     public Teacher createTeacher(@RequestBody Teacher teacher) {
         return teacherRepository.save(teacher);
+    }
+
+    @GetMapping("/teachers/{id}")
+    public ResponseEntity<Teacher> getTeacherById(@PathVariable Long id){
+        Teacher teacher = teacherRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Teacher with given ID does not exist"));
+        return ResponseEntity.ok(teacher);
+    }
+
+    @PutMapping("/teachers/{id}")
+    public ResponseEntity<Teacher> updateTeacher(@PathVariable Long id, @RequestBody Teacher teacherDetails) {
+        Teacher teacher = teacherRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Teacher with given ID does not exist"));
+
+        teacher.setName(teacherDetails.getName());
+        teacher.setLastName(teacherDetails.getLastName());
+        teacher.setEmail(teacherDetails.getEmail());
+
+        Teacher updatedTeacher = teacherRepository.save(teacher);
+
+        return ResponseEntity.ok(updatedTeacher);
+    }
+
+    @DeleteMapping("/teachers/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteTeacher(@PathVariable Long id){
+        Teacher teacher = teacherRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Teacher with given ID does not exist"));
+
+        teacherRepository.delete(teacher);
+        Map<String, Boolean> result = new HashMap<>();
+        result.put("deleted",Boolean.TRUE);
+        return ResponseEntity.ok(result);
     }
 }
